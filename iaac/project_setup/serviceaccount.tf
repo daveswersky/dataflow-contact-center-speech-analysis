36dcfbd4ad03f610df0b00c5738bc1cce3ebb3a4
dswersky <dswersky@gmail.com>
2022-02-02 12:34:29 -0600

    Fixed role bindings in project_setup

--- iaac/project_setup/serviceaccount.tf
+++ iaac/project_setup/serviceaccount.tf
@@ -1,25 +1,25 @@
 
 resource "google_service_account" "service_account" {
-  account_id   = "serviceAccount:project-service-account@${var.project_id}.iam.gserviceaccount.com"
+  account_id   = "serviceAccount:${var.project_id}-project-service-account"
   display_name = "Project Service Account"
 }
 
 resource "google_project_iam_member" "bigquery-binding" {
   project = var.project_id
   role    = "roles/bigquery.dataEditor"
-  member  = google_service_account.service_account.email
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "pubsub-binding" {
   project = var.project_id
   role    = "roles/pubsub.editor"
-  member  = google_service_account.service_account.email
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "dataflow-binding" {
   project = var.project_id
   role    = "roles/dataflow.worker"
-  member  = google_service_account.service_account.email
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "monitoring-binding" {
@@ -31,11 +31,11 @@ resource "google_project_iam_member" "monitoring-binding" {
 resource "google_project_iam_member" "storage-binding" {
   project = var.project_id
   role    = "roles/storage.admin"
-  member  = google_service_account.service_account.email
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_project_iam_member" "dlp-binding" {
   project = var.project_id
   role    = "roles/dlp.user"
-  member  = google_service_account.service_account.email
+  member  = "serviceAccount:${google_service_account.service_account.email}"
 }
